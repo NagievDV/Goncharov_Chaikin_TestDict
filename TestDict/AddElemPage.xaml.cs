8ae40e2ff8cc2f3453c6a44ff6d813e6fd647bd6
Dmitriy Goncharov <nagievv@yanex.ru>
2024-03-31 04:25:15 +0300

    initial commit

---
+++ TestDict/AddElemPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestDict
+{
+    /// <summary>
+    /// Логика взаимодействия для AddElemPage.xaml
+    /// </summary>
+    public partial class AddElemPage : Page
+    {
+        private Terms term = new Terms();
+
+
+        public AddElemPage()
+        {
+            InitializeComponent();
+
+            DataContext = term;
+        }
+
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void btnAddElem_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(term.Term))
+                errors.AppendLine("Укажите термин!");
+            if (string.IsNullOrWhiteSpace(term.Description))
+                errors.AppendLine("Укажите определение!");
+            if (string.IsNullOrWhiteSpace(term.Source))
+                errors.AppendLine("Укажите источник");
+            if (errors.Length > 0)
+            { 
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if(term.ID == 0) Entities.GetContext().Terms.Add(term);
+
+            try
+            {
+                Entities.GetContext().SaveChanges();
+                MessageBox.Show("Данные успешно сохранены!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+            NavigationService.GoBack();
+        }
+
+
+    }
+}
---
+++ TestDict/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestDict
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            dgridTerms.ItemsSource = Entities.GetContext().Terms.ToList();
+        }
+
+        private void btnDelElem(object sender, RoutedEventArgs e)
+        {
+            var usersForRemoving = dgridTerms.SelectedItems.Cast<Terms>().ToList();
+            if (MessageBox.Show($"Вы точно хотите удалить записи в количестве {usersForRemoving.Count()} элементов ? ",
+                "Внимание ",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+
+                try
+                {
+                    Entities.GetContext().Terms.RemoveRange(usersForRemoving);
+                    Entities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные успешно удалены!");
+                    dgridTerms.ItemsSource =
+                    Entities.GetContext().Terms.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void btnAddElem(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddElemPage());
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                Entities.GetContext().ChangeTracker.Entries().ToList().ForEach(x => x.Reload());
+
+                dgridTerms.ItemsSource = Entities.GetContext().Terms.ToList();
+            }
+        }
+    }
+}
